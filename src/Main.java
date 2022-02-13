0fae502536708e09187ad25e2f9834b83579033b
Sergey Shlyakhov <sergey.schlyakhov@ya.ru>
2022-02-13 21:44:58 +0300

    Step 2.4 Task 3 Step is completed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(mergeArrays(new int[]{1,2,3}, new int[]{1,3})));
+        String [] roles= {
+                "Городничий","Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич"};
+        String [] textLines={
+                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+        System.out.println(printTextPerRole(roles, textLines));
     }
 
     /**
@@ -71,5 +83,20 @@ public class Main {
         }
 
         return result;
+    }
+
+    private static String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder result = new StringBuilder();
+
+        for (String role : roles) {
+            result.append(role).append(":\n");
+            for (int TextIndex = 0; TextIndex < textLines.length; TextIndex++) {
+                if (textLines[TextIndex].startsWith(role + ":")) {
+                    result.append(TextIndex + 1).append(")").append(textLines[TextIndex].replaceFirst((role + ":"), "")).append("\n");
+                }
+            }
+            result.append("\n");
+        }
+        return result.toString();
     }
 }
