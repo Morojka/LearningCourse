dcc8f0339db228d13717b049824558b4f599d806
Sergey Shlyakhov <sergey.schlyakhov@ya.ru>
2022-04-27 11:54:18 +0300

    Task 5.2 done

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,25 @@ public class Main {
 
     }
 
+    //task 5.3
+    public static void rewriteStream() {
+        int currentResultsInput = -1, prevResultsInput = 0;
+        try (InputStream inputStream = System.in) {
+            do {
+                prevResultsInput = currentResultsInput;
+                currentResultsInput = inputStream.read();
+                if (prevResultsInput == 13 & currentResultsInput == 10) {
+                    continue;
+                } else if (prevResultsInput >= 0) {
+                    System.out.write(prevResultsInput);
+                    System.out.flush();
+                }
+            } while (currentResultsInput != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     //task 5.2
     //InputStream stream = new ByteArrayInputStream(new byte[] {0x33, 0x45, 0x01});
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
